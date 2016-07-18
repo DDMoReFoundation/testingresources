7184598d0deebd8d13283c1e8d15f6da47e06318
matsrogalski <mrogalski@mango-solutions.com>
2016-07-18 15:11:28 +0100

    DDMORE-1705: Remove references to removed simcyp.getResultsDirectory.

--- Demonstrator/mdl-ide-test-projects/src/main/resources/Simcyp/test.multiplePopulations.R
+++ Demonstrator/mdl-ide-test-projects/src/main/resources/Simcyp/test.multiplePopulations.R
@@ -26,9 +26,6 @@ test_that("Generates a SIM-OBESE population of 100 individuals", {
             multiPop1_SO <- simcyp.generateMultiplePopulations(c(popIds[[2]], popIds[[10]]), c(75, 50))
 
             expect_false(class(multiPop1_SO)=="try-error", "Doesn't crash with errors")
-            resultsDir<-simcyp.getResultsDirectory()
-            expect_false(is.null(list.files(resultsDir)), "SOME content in results directory") 
-            expect_false(is.null(list.files(resultsDir,pattern="SO\\.xml$")), "SO file exists")
             expect_equal(length(multiPop1_SO@TaskInformation@ErrorMessages),0, "There are No errors in SO")
             expect_false(is.null(multiPop1_SO@Simulation$SimulationBlock@SimulatedProfiles[[1]]@data), "SimulatedProfiles' data is populated")
 
@@ -42,9 +39,6 @@ test_that("Don't embed data in standard output", {
                                                             popIds[[8]], popIds[[16]]), 250, FALSE)
 
             expect_false(class(multiPop2_SO)=="try-error", "Doesn't crash with errors")
-            resultsDir<-simcyp.getResultsDirectory()
-            expect_false(is.null(list.files(resultsDir)), "SOME content in results directory") 
-            expect_false(is.null(list.files(resultsDir,pattern="SO\\.xml$")), "SO file exists")
             expect_equal(length(multiPop2_SO@TaskInformation@ErrorMessages),0, "There are No errors in SO")
             expect_false(is.null(multiPop2_SO@Simulation$SimulationBlock@SimulatedProfiles[[1]]@data), "SimulatedProfiles' data is populated")
 
